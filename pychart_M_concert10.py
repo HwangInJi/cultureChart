08e5046763ed3caa23e7931bf6bd86a1aef39b8e
Hwang InJi <163365140+HwangInJi@users.noreply.github.com>
2024-05-02 19:33:43 +0900

    Update pychart_M_concert10.py

--- pychart_M_concert10.py
+++ pychart_M_concert10.py
@@ -1,23 +1,22 @@
-import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 from datetime import datetime
 import time
+import json
 
-# 현재 날짜 가져오기
-current_date = datetime.now().strftime("%Y-%m-%d")
-folder_path = "melonconcert"
-filename = f"{folder_path}/chart_M_concert10_{current_date}.json"
 
 # 웹 드라이버 설정
 options = ChromeOptions()
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
-service = ChromeService(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
 url = "https://ticket.melon.com/ranking/index.htm"
 
 # 웹 페이지에 접속
@@ -36,19 +35,20 @@ for row in rows:
     title = row.find_element(By.CSS_SELECTOR, 'div.show_infor p.infor_text a').text.strip()
     image = row.find_element(By.CSS_SELECTOR, 'div.thumb_90x125 img').get_attribute('src')
     venue = row.find_element(By.CSS_SELECTOR, 'td:nth-child(4)').text.strip()
+
     data.append({
         'Rank': rank,
         'Title': title,
         'Venue': venue,
         'ImageURL': image
     })
-    
+
 # JSON 파일로 저장
 current_date = datetime.now().strftime("%Y-%m-%d")
-filename = f"melonconcert/melon_ticket_ranking_{current_date}.json"
+filename = f"melonconcert/pychart_M_concert10.py_{current_date}.json"
 with open(filename, 'w', encoding='utf-8') as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
-    
+
 # 드라이버 종료
 driver.quit()
 
